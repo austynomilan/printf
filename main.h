a268ae2eedc6be0d681e42aa5f5eedc062937c3e
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-07-25 19:05:28 +0100

    updated

--- main.h
+++ main.h
@@ -7,7 +7,8 @@ int _printf(const char *format, ...);
 int print_char(int c);
 int print_string(char *s);
 int print_percent();
-int print_binary(unsigned int num);
+int print_binary(int num);
 int print_pointer(void *ptr);
+int print_integer(int num)
 
 #endif
