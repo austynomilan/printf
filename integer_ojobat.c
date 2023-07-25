1e360098f91d60ef5f68098e9089cbfb6100b5ba
Babatunde Ojomu <babatundeojomu2016@gmail.com>
2023-07-25 19:27:33 +0100

    updated

--- integer_ojobat.c
+++ integer_ojobat.c
@@ -6,24 +6,27 @@
  * Return: The number characters printed
  */
 
-int print_integer(int num);
+int print_integer(int num)
+#define BUFFER_SIZE 1024
+
 {
+	int i;
 	int index = 0;
 	int adder = 0;
 	char buffer[BUFFER_SIZE];
 
 	if (num < 0)
 	{
-		print_char('-');
+		putchar('-');
 		num = -num;
 	}
 	do {
 		buffer[index++] = num % 10 + '0';
 		num /= 10;
 	} while (num > 0);
-	for (int i = index - 1; i >= 0; i--)
+	for (i = index - 1; i >= 0; i--)
 	{
-		adder += print_char(buffer[i]);
+		adder += putchar(buffer[i]);
 	}
 	return (adder);
 }
